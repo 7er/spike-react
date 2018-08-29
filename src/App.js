23da744c249d7cce86aa6ebbcdf67242eb5de2af
Syver Enstad <syver.enstad@telenor.com>
2018-08-29 16:20:46 +0200

    A rudimentary web application with most functionality stubbed out

--- src/App.js
+++ src/App.js
@@ -2,56 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { createStore } from 'redux'
-import todoApp from './reducers'
 
-import {
-  addTodo,
-  toggleTodo,
-  setVisibilityFilter,
-  VisibilityFilters
-} from './actions'
-
+import VisibleTodoList from './containers/VisibleTodoList'
 
 
 class App extends Component {
   render() {
     return (
-        <div className="App">
-        </div>
+        <VisibleTodoList />
     );
   }
 }
-
-
-
-
-
-const store = createStore(todoApp)
-
-
-// Log the initial state
-console.log('Initial state', store.getState())
-
-// Every time the state changes, log it
-// Note that subscribe() returns a function for unregistering the listener
-const unsubscribe = store.subscribe(
-    () => console.log('on state change', store.getState())
-)
-
-// Dispatch some actions
-store.dispatch(
-    addTodo('Learn about actions')
-    //{type: 'ADD_TODO', text: 'Learn about actions'}
-)
-store.dispatch(addTodo('Learn about reducers'))
-store.dispatch(addTodo('Learn about store'))
-store.dispatch(toggleTodo(0))
-store.dispatch(toggleTodo(1))
-store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
-
-// Stop listening to state updates
-unsubscribe()
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import todoApp from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import {
+  addTodo,
+  toggleTodo,
+  setVisibilityFilter,
+    VisibilityFilters
+} from './actions'
+
+const store = createStore(todoApp)
+
+store.dispatch(addTodo('Learn about react'))
+store.dispatch(addTodo('Learn about reducers'))
+store.dispatch(addTodo('Learn about store'))
+store.dispatch(toggleTodo(0))
+store.dispatch(toggleTodo(1))
+// store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'))
+
 registerServiceWorker();
